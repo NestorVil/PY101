12b966d7396027ab9e6d20e4038dfaa341d2661e
NestorV <nestorvilla571@gmail.com>
2025-01-21 14:53:18 -0800

    calculator.py refactor with json

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -3,6 +3,7 @@
 # Ask the user for the operation to preform.
 # Preform the operation on the two numbers.
 # Print the result to the terminal.
+import json
 
 def prompt(message):
     print(f"==> {message}")
@@ -12,41 +13,59 @@ def invalid_number(number_str):
         float(number_str)
     except ValueError:
         return True
-    
+
     return False
 
-prompt("Welcome to Calculator!")
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
 
-prompt("What's the first number?:")
-number1 = input()
+with open("calculator_message.json", "r") as file:
+    MESSAGES = json.load(file)
 
-while invalid_number(number1):
-    prompt(f"{number1} is not valid.")
+prompt(messages("welcome"))
+
+running = True
+
+while running:
+    prompt(messages("number_prompt_1"))
     number1 = input()
 
-prompt("What's the second number?:")
-number2 = input()
+    while invalid_number(number1):
+        prompt(messages("invalid_number"))
+        prompt(messages("number_prompt_1"))
+        number1 = input()
 
-while invalid_number(number2):
-    prompt(f"{number2} is not valid.")
+    prompt(messages("number_prompt_2"))
     number2 = input()
 
-prompt("What operation would you like to preform?\n"
-      "1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
+    while invalid_number(number2):
+        prompt(messages("invalid_number"))
+        prompt(messages("number_prompt_2"))
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    prompt(messages("operation_prompt"))
     operation = input()
 
-match operation:  # 1 Add, 2 subtract, 3 multiply, 4 divide
-    case "1":
-        result = float(number1) + float(number2)
-    case "2":
-        result = float(number1) - float(number2)
-    case "3":
-        result = float(number1) * float(number2)
-    case "4":
-        result = float(number1) / float(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt(messages("invalid_operation"))
+        prompt(messages("operation_prompt"))
+        operation = input()
 
-prompt(f"The result is: {result:.2f}")
+    match operation:  # 1 Add, 2 subtract, 3 multiply, 4 divide
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    output = round(output, 2)
+
+    prompt(messages("result").format(output=output))
+
+    prompt(messages("another_operation"))
+    answer = input().lower()
+    if answer and answer[0].lower() != "y":
+        running = False
